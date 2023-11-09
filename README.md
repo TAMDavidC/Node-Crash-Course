3d72c8cc2065b28ff4f1011d3a37d237de015abb
TAMDavidC <dcruz43@ivytech.edu>
2023-11-09 10:05:51 -0500

    introduction 1 Completed

---
+++ README.md
@@ -0,0 +1,9 @@
+# learning how to use Node.js
+
+David cruz
+11/9/2023
+
+## Completed lessons
+
+https://youtu.be/zb3Qk8SG5Ms?si=xmbQ5Rt4zteyAXBr
+Node.js Crash Course tutorial #1 - introduction & Setup
---
+++ test.js
@@ -0,0 +1,9 @@
+const name = "yoshi";
+
+console.log(name);
+
+/* Notes
+To run a JS file in the terminal type "node {filename}" I.E. "node test".
+pressing Alt+/ comments things out...
+Correction it's Ctrl+/
+*/
